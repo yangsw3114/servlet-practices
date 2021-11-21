ebc092c92512f458510c66ff7a901f93d0d08a2e
yangsw3114 <72145880+yangsw3114@users.noreply.github.com>
2021-11-21 16:44:46 +0900

    sql

--- sql/guestbook.sql
+++ sql/guestbook.sql
@@ -3,7 +3,7 @@ desc guestbook;
 -- insert
 insert into guestbook values(null, '양승우', '1234', '하이',now());
 insert into guestbook values(null, '양아치', '1234', '뭘봐',now());
-insert into guestbook values(null, '안대혁', '1234', '안녕하세요',now());
+insert into guestbook values(null, '양승우2', '1234', '안녕하세요',now());
 
 -- select
 select no, name, date_format(reg_date,'%Y-%m-%d %H:%i:%s') as '시간', message
@@ -12,3 +12,8 @@ from guestbook order by reg_date asc;
 -- delete
 delete from guestbook where no=13 and  password ='1234';
 delete from guestbook;
+
+select * from guestbook;
+
+desc emaillist;
+select * from emaillist;
